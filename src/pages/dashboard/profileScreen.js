c2d9884a4bce8d7d982914571c0b203c3ef41261
sagartyag <sagartyagi1024@gmail.com>
2025-03-01 13:09:41 +0530

    ok

--- src/pages/dashboard/profileScreen.js
+++ src/pages/dashboard/profileScreen.js
@@ -1,15 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/footer';
 import { useNavigate } from "react-router-dom";
+import Api from "../../service/Api";
 
 
 function Account() {
   const [showPopup, setShowPopup] = useState(false);
-  const [username, setUsername] = useState("sagartyagi");
   const [showPopupLogout, setShowPopupLogout] = useState(false);
   const [selectedButton, setSelectedButton] = useState("no");
   const navigate = useNavigate(); // Redirect function
+   const [newName, setNewName] = useState(null); // User data store karne ke liye state
+     const [UserData, setUserData] = useState(null); // User data store karne ke liye state
+     const [user, setUser] = useState(null);
 
   const handleLogout = () => {
     localStorage.removeItem("token"); // Token remove
@@ -18,6 +21,40 @@ function Account() {
   };
 
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await Api.get("getUsername"); // API call to fetch user data
+        if (response.data) {
+          setNewName(response.data); 
+          setUserData(response.data.fullname); 
+
+
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+
+  const handleUpdateProfile = async () => {
+    try {
+        const response = await Api.put("updateFullName", { fullname: UserData });
+
+        if (response.data) {
+            setUser((prevUser) => ({ ...prevUser, fullname: UserData }));
+             console.log("Profile Updated Successfully");
+             
+        }
+       
+    } catch (error) {
+        console.error("Error updating profile:", error);
+    } 
+};
+  
   return (
     <div className="container bg-n900 min-h-dvh relative overflow-hidden flex justify-start items-start text-white pb-28">
     {/* Background Circle */}
@@ -33,7 +70,7 @@ function Account() {
   >
     <div 
       style={{ 
-        backgroundColor: "rgb(42 47 66)", 
+        backgroundColor: "rgb(32 37 57)", 
         width: 429, 
         height: 313,
         borderTopLeftRadius: "38px",
@@ -42,36 +79,40 @@ function Account() {
       className="fixed bottom-20 bg-n900 text-white p-6 shadow-lg rounded-t-lg transition-transform transform translate-y-0 z-30"
       onClick={(e) => e.stopPropagation()} // Yeh ensure karega ki popup par click karne se close na ho
     >
-      <h1 className="text-lg font-bold mb-4 text-center">
+      <div className="flex justify-center items-center">
+                <div style={{backgroundColor:"#fff"}} className="w-16 h-1 bg-white bg-opacity-5 rounded-full"></div>
+              </div>
+      <h1 style={{marginTop:"18px"}} className="text-lg font-bold mb-4 text-center">
         Edit Username
       </h1>
-      <p className="text-gray-500 text-center mb-4">
+      <p style={{marginTop:"10px",marginBottom:"20px"}} className="text-gray-500 text-center mb-4">
         Please fill in the field to continue
       </p>
 
       <form className="flex flex-col gap-3">
         <div className="relative w-full">
-          <i className="ph ph-user absolute inset-y-0 left-3 flex items-center text-white text-lg"></i>
+          <i style={{marginTop:"24px",marginLeft:"10px"}} className="ph ph-user absolute inset-y-0 left-3 flex items-center text-white text-lg"></i>
 
           <input  
             style={{ 
               backgroundColor: "rgb(88 102 89)", 
               color: "#fff", 
               borderColor: "#292c3d",
-              paddingLeft: "40px" // Left padding taaki text icon se overlap na ho
+              paddingLeft: "40px", // Left padding taaki text icon se overlap na ho
+marginTop:"13px"
             }}
             type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={UserData}        
+            onChange={(e) => setUserData(e.target.value)} 
             className="w-full p-3 border rounded-md text-black"
             required
           />
         </div>
 
-        <button
-          style={{ backgroundColor: "#9583ff" }}
+        <button onClick={handleUpdateProfile} 
+          style={{ backgroundColor: "#9583ff",marginTop:"16px" }}
           type="submit"
-          className="w-full p-3 bg-purple-500 text-white rounded-md hover:bg-purple-600"
+          className="w-full p-3 font-bold mb-4 bg-purple-500 text-white rounded-md hover:bg-purple-600"
         >
           Save Changes
         </button>
@@ -86,21 +127,23 @@ function Account() {
 {/* Logout Popup */}
 {showPopupLogout && (
         <div
-          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
+          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end z-20"
           onClick={() => setShowPopupLogout(false)} // Click outside to close
         >
           <div
-            style={{ backgroundColor: "#202338", width: 425, height: 384, marginBottom: -39, borderTopLeftRadius: "38px",
+            style={{ backgroundColor: "#202338", width: 425, height: 351, marginBottom: -39, borderTopLeftRadius: "38px",
               borderTopRightRadius: "38px" }}
             className="fixed bottom-10 bg-n900 text-white p-6 shadow-lg rounded-t-lg transition-transform transform translate-y-0"
             onClick={(e) => e.stopPropagation()} // Prevent closing on click inside
           >
             {/* Info Icon */}
             <div className="h-1 w-16 bg-gray-300 rounded mx-auto mb-3"></div>
-
+            <div className="flex justify-center items-center">
+                <div style={{backgroundColor:"#fff"}} className="w-16 h-1 bg-white bg-opacity-5 rounded-full"></div>
+              </div>
             {/* Rounded Image */}
             <div className="flex justify-center mb-4">
-              <img
+              <img style={{marginTop:"36px"}}
                 src="/assets/images/dc113b39-e659-4e08-be93-ca29058dc0dc.webp"
                 alt="Info"
                 className="w-16 h-16 rounded-full"
@@ -119,7 +162,7 @@ function Account() {
                   backgroundColor: selectedButton === "yes" ? "#9583ff" : "white",
                   color: selectedButton === "yes" ? "white" : "black",
                   border: "1px solid gray",
-                  borderRadius:"28px"
+                  borderRadius:"12px"
                 }}
                 className="w-full py-3 font-semibold rounded"
                 onClick={handleLogout} // Logout on Yes click
@@ -130,7 +173,7 @@ function Account() {
               <button
                 style={{
                   backgroundColor: selectedButton === "no" ? "#9583ff" : "white",
-                  color: selectedButton === "no" ? "white" : "black",borderRadius:"28px"
+                  color: selectedButton === "no" ? "white" : "black",borderRadius:"12px",marginTop:"14px"
 
                 }}
                 className="w-full py-3 font-semibold rounded"
@@ -157,8 +200,7 @@ function Account() {
     <img alt="Profile picture of a person with sunglasses" className="w-12 h-12 rounded-full" height="50" src="\assets\images\userIcon.edc1c75ce595e5bb3b239b6d69ec9cf4.svg" width="50"/>
     <div style={{marginLeft:"10px"}} className="ml-4">
      <h2 className="text-lg font-bold">
-      Set Username
-     </h2>
+     {UserData}  </h2>
      <p style={{color:"rgba(169, 172, 175, 1)"}} className="text-gray-500">
       sagartyagi1024@gmail.com
      </p>
